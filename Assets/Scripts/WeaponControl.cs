044e36b981103a25e3f2cfab159fae995273b5be
TimeToKill82 <dean099099@gmail.com>
2023-11-15 20:01:38 +1300

    nearly finished bullet pen
    just need to reduce bullet speed and pen  val after pen

--- Assets/Scripts/WeaponControl.cs
+++ Assets/Scripts/WeaponControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class WeaponControl : MonoBehaviour
@@ -8,9 +9,9 @@ public class WeaponControl : MonoBehaviour
     public GameObject rayStartRef;
     public GameObject shootDirRef;
     public GameObject gun;
-    public float bulletVelocity;
-    public float timeBetweenStages = 0.5f;
-    public float bulletGravity = 1f;
+    public float stageLength = 0.001f;
+    private float timeBetweenStages = 0.1f;
+    public float bulletGravity = 2f;
     private float measureSectionLength = 0.001f;
     public float bulletPenVal;
 
@@ -24,11 +25,9 @@ public class WeaponControl : MonoBehaviour
         //declaring local variables so there can be multiple bullets at once
         Vector3 rayDir;
         Vector3 rayStart;
-        Vector3 initialHit;
-        Vector3 endHit = new Vector3(0,0,0);
         RaycastHit hitInfo;
-        float objectThickness = 0;
-        bool measureFinished;
+        float modTimeBetweenStages = timeBetweenStages;
+        float modBulletPenVal = bulletPenVal;
         GameObject grah = new GameObject();
         //setting the locations and values of vector3s / gameobjects
         grah.transform.rotation = gun.transform.rotation;
@@ -38,8 +37,8 @@ public class WeaponControl : MonoBehaviour
         while (true)
         {
             //draw ray for debug and if statement with raycast code
-            Debug.DrawRay(rayStart, rayDir, Color.red, 5);
-            if (Physics.Raycast(rayStart, rayDir,out hitInfo, bulletVelocity))
+            Debug.DrawRay(rayStart, rayDir.normalized * stageLength, Color.red, 5);
+            if (Physics.Raycast(rayStart, rayDir,out hitInfo, stageLength))
             {
                 //damage ang debug
                 Debug.Log("hit");
@@ -47,57 +46,60 @@ public class WeaponControl : MonoBehaviour
                 {
                     processDamage();
                 }
-                //start of measure system for bullet pen
-                //declaring values and some debug
-                measureFinished = false;
-                initialHit = hitInfo.point;
-                endHit = initialHit + (rayDir.normalized * measureSectionLength);
-                //main measure loop
-                while (measureFinished == false)
+                Debug.Log(measureThickness(hitInfo, rayDir, modBulletPenVal));
+                if (measureThickness(hitInfo, rayDir, modBulletPenVal) == false)
                 {
-                    //Debug.Log(measureFinished);
-                    //moving point for measure think "extending the tapemeasure"
-                    endHit = endHit + (rayDir.normalized * measureSectionLength);
-                    //debug stuff
-                    grah.transform.position = endHit;
-                    //Debug.Log(endHit);
-                    //declares and sets colliders used to figure out if the point(end of tape measure) is in an object
-                    Collider[] hitColliders = Physics.OverlapSphere(endHit, 0f);
-                    //Debug.Log(hitColliders.Length);
-                    //checks if point(end of tape measure) is in an object
-                    if(hitColliders.Length == 0)
-                    {
-                        //sets the bool that controls loop to true and sets object thickness output
-                        measureFinished = true;
-                        objectThickness = Vector3.Distance(initialHit, endHit);
-                    }
-                }
-                //prints debug stuff and figures out if bulleet pens or not
-                Debug.Log(initialHit);
-                Debug.Log(endHit);
-                Debug.Log(objectThickness);
-                if (bulletPenVal > objectThickness)
-                {
-                    Debug.Log("Pen");
-                    yield break;
-                }
-                else
-                {
-                    Debug.Log("Non-Pen");
                     yield break;
                 }
             }
             //prepares values for next cycle of loop
-            grah.transform.position = rayStart + rayDir;
+            grah.transform.position = rayStart + rayDir.normalized * stageLength;
             rayStart = grah.transform.position;
             rayDir = grah.transform.forward;
             if (grah.transform.rotation.x != 90 || grah.transform.rotation.x < 90)
             {
-                grah.transform.Rotate(2, 0, 0);
+                grah.transform.Rotate(bulletGravity, 0, 0);
                 //Debug.Log("chess");
             }
             //waits for x seconds to control bullet velocity
-            yield return new WaitForSeconds(timeBetweenStages);
+            yield return new WaitForSeconds(modTimeBetweenStages);
+        }
+    }
+
+    private bool measureThickness(RaycastHit mHitInfo, Vector3 mRayDir, float mBulletPenVal)
+    {
+        Vector3 initialHit;
+        Vector3 endHit = new Vector3(0, 0, 0);
+        float objectThickness = 0;
+        bool measureFinished;
+        measureFinished = false;
+        initialHit = mHitInfo.point;
+        endHit = initialHit + (mRayDir.normalized * measureSectionLength);
+        while (measureFinished == false)
+        {
+            //Debug.Log(measureFinished);
+            //moving point for measure think "extending the tapemeasure"
+            endHit = endHit + (mRayDir.normalized * measureSectionLength);
+            //debug stuff
+            //Debug.Log(endHit);
+            //declares and sets colliders used to figure out if the point(end of tape measure) is in an object
+            Collider[] hitColliders = Physics.OverlapSphere(endHit, 0f);
+            //Debug.Log(hitColliders.Length);
+            //checks if point(end of tape measure) is in an object
+            if (hitColliders.Length == 0)
+            {
+                //sets the bool that controls loop to true and sets object thickness output
+                measureFinished = true;
+                objectThickness = Vector3.Distance(initialHit, endHit);
+            }
+        }
+        if (mBulletPenVal > objectThickness)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
